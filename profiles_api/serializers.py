19b1e33db5bc737ec5e3fae77e3233bf731d8523
Hamza LIMOURI <limouri.hamza@gmail.com>
2021-04-12 05:23:25 +0000

    Added profile viewset

---
+++ profiles_api/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+
+from profiles_api import models
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serializes a user profile object"""
+
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'first_name', 'last_name', 'password')
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {
+                    'input_field': 'password'
+                }
+            }
+        }
+
+    def create(self, validated_data):
+        """Create and return a new user"""
+        user = models.UserProfile.objects.create_user(
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            password=validated_data['password']
+        )
+        return user
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.db.models.query import QuerySet
+from rest_framework import viewsets, status
+from rest_framework.authentication import TokenAuthentication
 
-# Create your views here.
+from profiles_api import serializers, models, permissions
+
+
+class UserProfileViewSets(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (permissions.UpdateOwnProfile, )
